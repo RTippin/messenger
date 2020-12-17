11c9586197270d5dac598dbd47eba4b24262a775
Tippin <richard.tippin@gmail.com>
2020-12-17 00:34:02 -0500

    friend test

--- tests/Http/FriendsTest.php
+++ tests/Http/FriendsTest.php
@@ -2,6 +2,12 @@
 
 namespace RTippin\Messenger\Tests\Http;
 
+use RTippin\Messenger\Broadcasting\FriendApprovedBroadcast;
+use RTippin\Messenger\Broadcasting\FriendRequestBroadcast;
+use RTippin\Messenger\Contracts\FriendDriver;
+use RTippin\Messenger\Events\FriendApprovedEvent;
+use RTippin\Messenger\Events\FriendRequestEvent;
+use RTippin\Messenger\Models\PendingFriend;
 use RTippin\Messenger\Models\SentFriend;
 use RTippin\Messenger\Tests\FeatureTestCase;
 use RTippin\Messenger\Tests\UserModel;
@@ -45,6 +51,11 @@ class FriendsTest extends FeatureTestCase
         $users = UserModel::all();
 
         $this->actingAs($users->first());
+
+        $this->expectsEvents([
+            FriendRequestBroadcast::class,
+            FriendRequestEvent::class,
+        ]);
 
         $response = $this->postJson(route('api.messenger.friends.sent.store'), [
             'recipient_id' => $users->last()->id,
@@ -102,12 +113,96 @@ class FriendsTest extends FeatureTestCase
         $this->actingAs($users->first());
 
         $this->deleteJson(route('api.messenger.friends.sent.destroy', [
-            'sent' => $sent->id
+            'sent' => $sent->id,
         ]))->assertSuccessful();
 
         $this->assertDatabaseMissing('pending_friends', [
             'sender_id' => $users->first()->id,
             'recipient_id' => $users->last()->id,
         ]);
+    }
+
+    /** @test */
+    public function test_user_can_deny_pending_request()
+    {
+        $users = UserModel::all();
+
+        $pending = PendingFriend::create([
+            'sender_id' => $users->first()->id,
+            'sender_type' => 'RTippin\Messenger\Tests\UserModel',
+            'recipient_id' => $users->last()->id,
+            'recipient_type' => 'RTippin\Messenger\Tests\UserModel',
+        ]);
+
+        $this->assertDatabaseHas('pending_friends', [
+            'sender_id' => $users->first()->id,
+            'recipient_id' => $users->last()->id,
+        ]);
+
+        $this->actingAs($users->first());
+
+        $this->deleteJson(route('api.messenger.friends.pending.destroy', [
+            'pending' => $pending->id,
+        ]))->assertForbidden();
+
+        $this->actingAs($users->last());
+
+        $this->deleteJson(route('api.messenger.friends.pending.destroy', [
+            'pending' => $pending->id,
+        ]))->assertSuccessful();
+
+        $this->assertDatabaseMissing('pending_friends', [
+            'sender_id' => $users->first()->id,
+            'recipient_id' => $users->last()->id,
+        ]);
+    }
+
+    /** @test */
+    public function test_user_can_accept_pending_request()
+    {
+        $users = UserModel::all();
+
+        $friends = resolve(FriendDriver::class);
+
+        $this->expectsEvents([
+            FriendApprovedBroadcast::class,
+            FriendApprovedEvent::class,
+        ]);
+
+        $pending = SentFriend::create([
+            'sender_id' => $users->first()->id,
+            'sender_type' => 'RTippin\Messenger\Tests\UserModel',
+            'recipient_id' => $users->last()->id,
+            'recipient_type' => 'RTippin\Messenger\Tests\UserModel',
+        ]);
+
+        $this->actingAs($users->first());
+
+        $this->putJson(route('api.messenger.friends.pending.update', [
+            'pending' => $pending->id,
+        ]))->assertForbidden();
+
+        $this->actingAs($users->last());
+
+        $this->putJson(route('api.messenger.friends.pending.update', [
+            'pending' => $pending->id,
+        ]))->assertSuccessful();
+
+        $this->assertDatabaseMissing('pending_friends', [
+            'sender_id' => $users->first()->id,
+            'recipient_id' => $users->last()->id,
+        ]);
+
+        $this->assertDatabaseHas('friends', [
+            'owner_id' => $users->first()->id,
+            'party_id' => $users->last()->id,
+        ]);
+
+        $this->assertDatabaseHas('friends', [
+            'owner_id' => $users->last()->id,
+            'party_id' => $users->first()->id,
+        ]);
+
+        $this->assertEquals($friends->friendStatus($users->first()), 1);
     }
 }
