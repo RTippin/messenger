ef9ce86c3d51506495f362382c4ada0844b5b32f
Tippin <richard.tippin@gmail.com>
2021-01-05 03:16:42 -0500

    invite join test pass except when existing invite and disabled in config. fixing

--- tests/Http/JoinWithInviteTest.php
+++ tests/Http/JoinWithInviteTest.php
@@ -2,6 +2,9 @@
 
 namespace RTippin\Messenger\Tests\Http;
 
+use Illuminate\Support\Facades\Event;
+use RTippin\Messenger\Events\InviteUsedEvent;
+use RTippin\Messenger\Facades\Messenger;
 use RTippin\Messenger\Models\Invite;
 use RTippin\Messenger\Models\Thread;
 use RTippin\Messenger\Tests\FeatureTestCase;
@@ -151,5 +154,93 @@ class JoinWithInviteTest extends FeatureTestCase
                     'is_valid' => true,
                 ],
             ]);
+    }
+
+    /** @test */
+    public function non_participant_can_join_group_with_valid_invite()
+    {
+        Event::fake([
+            InviteUsedEvent::class,
+        ]);
+
+        $smith = $this->createJaneSmith();
+
+        $this->actingAs($smith);
+
+        $this->postJson(route('api.messenger.invites.join', [
+            'invite' => 'TEST1234',
+        ]))
+            ->assertSuccessful()
+            ->assertJson([
+                'thread_id' => $this->group->id,
+            ]);
+
+        $this->assertDatabaseHas('participants', [
+            'thread_id' => $this->group->id,
+            'owner_id' => $smith->getKey(),
+            'owner_type' => get_class($smith),
+        ]);
+
+        $this->assertEquals(3, $this->group->participants()->count());
+
+        Event::assertDispatched(function (InviteUsedEvent $event) use($smith) {
+            $this->assertEquals($smith->getKey(), $event->provider->getKey());
+            $this->assertEquals($this->group->id, $event->thread->id);
+            $this->assertEquals($this->invite->id, $event->invite->id);
+
+            return true;
+        });
+    }
+
+    /** @test */
+    public function forbidden_to_join_group_with_valid_invite_when_disabled_from_group_settings()
+    {
+        $this->group->update([
+            'invitations' => false,
+        ]);
+
+        $this->actingAs($this->companyDevelopers());
+
+        $this->postJson(route('api.messenger.invites.join', [
+            'invite' => 'TEST1234',
+        ]))
+            ->assertForbidden();
+    }
+
+    /** @test */
+    public function forbidden_to_join_group_with_valid_invite_when_disabled_from_config()
+    {
+        Messenger::setThreadInvites(false);
+
+        $this->actingAs($this->companyDevelopers());
+
+        $this->postJson(route('api.messenger.invites.join', [
+            'invite' => 'TEST1234',
+        ]))
+            ->assertForbidden();
+    }
+
+    /** @test */
+    public function existing_participant_forbidden_to_join_group_with_valid_invite()
+    {
+        $this->actingAs($this->userDoe());
+
+        $this->postJson(route('api.messenger.invites.join', [
+            'invite' => 'TEST1234',
+        ]))
+            ->assertForbidden();
+    }
+
+    /** @test */
+    public function forbidden_to_join_group_with_expired_but_not_deleted_invite()
+    {
+        $this->travel(2)->hours();
+
+        $this->actingAs($this->companyDevelopers());
+
+        $this->postJson(route('api.messenger.invites.join', [
+            'invite' => 'TEST1234',
+        ]))
+            ->assertForbidden();
     }
 }
