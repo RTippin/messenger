a609b8e386ae006177b15fd16f34f93c0fd6db8f
Tippin <richard.tippin@gmail.com>
2021-01-09 01:38:13 -0500

    Call heartbeat test pass

--- tests/Http/CallHeartbeatTest.php
+++ tests/Http/CallHeartbeatTest.php
@@ -2,6 +2,7 @@
 
 namespace RTippin\Messenger\Tests\Http;
 
+use Illuminate\Support\Facades\Cache;
 use RTippin\Messenger\Models\Call;
 use RTippin\Messenger\Models\Thread;
 use RTippin\Messenger\Tests\FeatureTestCase;
@@ -27,5 +28,91 @@ class CallHeartbeatTest extends FeatureTestCase
             $this->group,
             $tippin
         );
+    }
+
+    /** @test */
+    public function heartbeat_cannot_be_a_post()
+    {
+        $this->actingAs($this->userTippin());
+
+        $this->postJson(route('api.messenger.threads.calls.heartbeat', [
+            'thread' => $this->group->id,
+            'call' => $this->call->id,
+        ]))
+            ->assertStatus(405);
+    }
+
+    /** @test */
+    public function heartbeat_on_missing_call_not_found()
+    {
+        $this->actingAs($this->userTippin());
+
+        $this->getJson(route('api.messenger.threads.calls.heartbeat', [
+            'thread' => $this->group->id,
+            'call' => '123-456-789',
+        ]))
+            ->assertNotFound();
+    }
+
+    /** @test */
+    public function non_call_participant_forbidden_to_use_heartbeat()
+    {
+        $this->actingAs($this->userDoe());
+
+        $this->getJson(route('api.messenger.threads.calls.heartbeat', [
+            'thread' => $this->group->id,
+            'call' => $this->call->id,
+        ]))
+            ->assertForbidden();
+    }
+
+    /** @test */
+    public function inactive_participant_forbidden_to_use_heartbeat()
+    {
+        $this->call->participants()
+            ->first()
+            ->update([
+                'left_call' => now(),
+            ]);
+
+        $this->actingAs($this->userTippin());
+
+        $this->getJson(route('api.messenger.threads.calls.heartbeat', [
+            'thread' => $this->group->id,
+            'call' => $this->call->id,
+        ]))
+            ->assertForbidden();
+    }
+
+    /** @test */
+    public function forbidden_to_use_heartbeat_on_inactive_call()
+    {
+        $this->call->update([
+            'call_ended' => now(),
+        ]);
+
+        $this->actingAs($this->userTippin());
+
+        $this->getJson(route('api.messenger.threads.calls.heartbeat', [
+            'thread' => $this->group->id,
+            'call' => $this->call->id,
+        ]))
+            ->assertForbidden();
+    }
+
+    /** @test */
+    public function call_participant_can_use_heartbeat()
+    {
+        $participant = $this->call->participants()->first();
+
+        $this->actingAs($this->userTippin());
+
+        $this->getJson(route('api.messenger.threads.calls.heartbeat', [
+            'thread' => $this->group->id,
+            'call' => $this->call->id,
+        ]))
+            ->assertSuccessful();
+
+        $this->assertTrue(Cache::has("call:{$this->call->id}:{$participant->id}"));
     }
 }
