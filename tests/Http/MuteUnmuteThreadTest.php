f9f51f834b3e4bdbc7a692b9e3e2a8651a52c405
Tippin <richard.tippin@gmail.com>
2021-01-13 21:48:19 -0500

    mute/unmute test pass. Going to add event on action class now

--- tests/Http/MuteUnmuteThreadTest.php
+++ tests/Http/MuteUnmuteThreadTest.php
@@ -2,6 +2,9 @@
 
 namespace RTippin\Messenger\Tests\Http;
 
+use Illuminate\Support\Facades\Event;
+use RTippin\Messenger\Broadcasting\NewMessageBroadcast;
+use RTippin\Messenger\Events\NewMessageEvent;
 use RTippin\Messenger\Models\Thread;
 use RTippin\Messenger\Tests\FeatureTestCase;
 
@@ -39,5 +42,93 @@ class MuteUnmuteThreadTest extends FeatureTestCase
             'thread' => $this->private->id,
         ]))
             ->assertForbidden();
+    }
+
+    /** @test */
+    public function participant_can_mute_thread()
+    {
+        $tippin = $this->userTippin();
+
+        $this->actingAs($tippin);
+
+        $this->postJson(route('api.messenger.threads.mute', [
+            'thread' => $this->private->id,
+        ]))
+            ->assertSuccessful();
+
+        $this->assertDatabaseHas('participants', [
+            'owner_id' => $tippin->getKey(),
+            'owner_type' => get_class($tippin),
+            'thread_id' => $this->private->id,
+            'muted' => true,
+        ]);
+    }
+
+    /** @test */
+    public function participant_can_unmute_thread()
+    {
+        $tippin = $this->userTippin();
+
+        $this->private->participants()
+            ->where('owner_id', '=', $tippin->getKey())
+            ->where('owner_type', '=', get_class($tippin))
+            ->first()
+            ->update([
+                'muted' => true,
+            ]);
+
+        $this->actingAs($tippin);
+
+        $this->postJson(route('api.messenger.threads.unmute', [
+            'thread' => $this->private->id,
+        ]))
+            ->assertSuccessful();
+
+        $this->assertDatabaseHas('participants', [
+            'owner_id' => $tippin->getKey(),
+            'owner_type' => get_class($tippin),
+            'thread_id' => $this->private->id,
+            'muted' => false,
+        ]);
+    }
+
+    /** @test */
+    public function muted_participant_receives_no_broadcast()
+    {
+        $doe = $this->userDoe();
+
+        $tippin = $this->userTippin();
+
+        $this->private->participants()
+            ->where('owner_id', '=', $doe->getKey())
+            ->where('owner_type', '=', get_class($doe))
+            ->first()
+            ->update([
+                'muted' => true,
+            ]);
+
+        Event::fake([
+            NewMessageBroadcast::class,
+            NewMessageEvent::class,
+        ]);
+
+        $this->actingAs($tippin);
+
+        $this->postJson(route('api.messenger.threads.messages.store', [
+            'thread' => $this->private->id,
+        ]), [
+            'message' => 'Hello!',
+            'temporary_id' => '123-456-789',
+        ])
+            ->assertSuccessful();
+
+        Event::assertDispatched(function (NewMessageBroadcast $event) use ($doe, $tippin) {
+            $this->assertNotContains('private-user.'.$doe->getKey(), $event->broadcastOn());
+            $this->assertContains('private-user.'.$tippin->getKey(), $event->broadcastOn());
+
+            return true;
+        });
+
+        Event::assertDispatched(NewMessageEvent::class);
     }
 }
