1fd8e46e935f8a175f472a6c1415b15a6386e0c4
Tippin <richard.tippin@gmail.com>
2021-01-16 01:26:02 -0500

    DestroyMessengerAvatarTest.php tests pass

---
+++ tests/Actions/DestroyMessengerAvatarTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace RTippin\Messenger\Tests\Actions;
+
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use RTippin\Messenger\Actions\DestroyMessengerAvatar;
+use RTippin\Messenger\Facades\Messenger;
+use RTippin\Messenger\Tests\FeatureTestCase;
+
+class DestroyMessengerAvatarTest extends FeatureTestCase
+{
+    /** @test */
+    public function destroy_avatar_removes_file_and_updates_provider()
+    {
+        $tippin = $this->userTippin();
+
+        $directory = Messenger::getAvatarStorage('directory').'/user/'.$tippin->getKey();
+
+        Messenger::setProvider($tippin);
+
+        Storage::fake(Messenger::getAvatarStorage('disk'));
+
+        $tippin->update([
+            'picture' => 'avatar.jpg',
+        ]);
+
+        UploadedFile::fake()
+            ->image('avatar.jpg')
+            ->storeAs($directory, 'avatar.jpg', [
+                'disk' => Messenger::getAvatarStorage('disk'),
+            ]);
+
+        app(DestroyMessengerAvatar::class)->execute();
+
+        $this->assertNull($tippin->picture);
+
+        Storage::disk(Messenger::getAvatarStorage('disk'))
+            ->assertMissing($directory.'/avatar.jpg');
+    }
+}
