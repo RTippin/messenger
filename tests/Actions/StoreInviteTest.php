fdba401fa2bed40d0bc7da23752d60b4e3f71921
Tippin <richard.tippin@gmail.com>
2021-01-18 17:54:55 -0500

    StoreInviteTest.php created, passes

---
+++ tests/Actions/StoreInviteTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace RTippin\Messenger\Tests\Actions;
+
+use Illuminate\Support\Facades\Event;
+use RTippin\Messenger\Actions\Invites\StoreInvite;
+use RTippin\Messenger\Contracts\MessengerProvider;
+use RTippin\Messenger\Events\NewInviteEvent;
+use RTippin\Messenger\Facades\Messenger;
+use RTippin\Messenger\Models\Thread;
+use RTippin\Messenger\Tests\FeatureTestCase;
+
+class StoreInviteTest extends FeatureTestCase
+{
+    private Thread $group;
+
+    private MessengerProvider $tippin;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->tippin = $this->userTippin();
+
+        $this->group = $this->createGroupThread($this->tippin);
+
+        Messenger::setProvider($this->tippin);
+    }
+
+    /** @test */
+    public function store_invite_stores_invite()
+    {
+        app(StoreInvite::class)->withoutDispatches()->execute(
+            $this->group,
+            [
+                'expires' => 0,
+                'uses' => 0,
+            ]
+        );
+
+        $this->assertDatabaseHas('thread_invites', [
+            'thread_id' => $this->group->id,
+            'max_use' => 0,
+            'expires_at' => null,
+        ]);
+    }
+
+    /** @test */
+    public function store_invite_fires_event()
+    {
+        Event::fake([
+            NewInviteEvent::class,
+        ]);
+
+        app(StoreInvite::class)->execute(
+            $this->group,
+            [
+                'expires' => 0,
+                'uses' => 0,
+            ]
+        );
+
+        Event::assertDispatched(function (NewInviteEvent $event) {
+            return $this->group->id === $event->invite->thread_id;
+        });
+    }
+}
--- tests/Http/InvitesTest.php
+++ tests/Http/InvitesTest.php
@@ -2,7 +2,7 @@
 
 namespace RTippin\Messenger\Tests\Http;
 
-use Illuminate\Support\Facades\Event;
+use RTippin\Messenger\Contracts\MessengerProvider;
 use RTippin\Messenger\Events\InviteArchivedEvent;
 use RTippin\Messenger\Events\NewInviteEvent;
 use RTippin\Messenger\Facades\Messenger;
@@ -16,18 +16,24 @@ class InvitesTest extends FeatureTestCase
 
     private Invite $invite;
 
+    private MessengerProvider $tippin;
+
+    private MessengerProvider $doe;
+
     protected function setUp(): void
     {
         parent::setUp();
 
-        $tippin = $this->userTippin();
+        $this->tippin = $this->userTippin();
 
-        $this->group = $this->createGroupThread($tippin, $this->userDoe());
+        $this->doe = $this->userDoe();
+
+        $this->group = $this->createGroupThread($this->tippin, $this->doe);
 
         $this->invite = $this->group->invites()
             ->create([
-                'owner_id' => $tippin->getKey(),
-                'owner_type' => get_class($tippin),
+                'owner_id' => $this->tippin->getKey(),
+                'owner_type' => get_class($this->tippin),
                 'code' => 'TEST1234',
                 'max_use' => 1,
                 'uses' => 0,
@@ -38,14 +44,9 @@ class InvitesTest extends FeatureTestCase
     /** @test */
     public function forbidden_to_view_invites_on_private_thread()
     {
-        $tippin = $this->userTippin();
+        $private = $this->createPrivateThread($this->tippin, $this->doe);
 
-        $private = $this->createPrivateThread(
-            $tippin,
-            $this->userDoe()
-        );
-
-        $this->actingAs($tippin);
+        $this->actingAs($this->tippin);
 
         $this->getJson(route('api.messenger.threads.invites.index', [
             'thread' => $private->id,
@@ -67,7 +68,7 @@ class InvitesTest extends FeatureTestCase
     /** @test */
     public function participant_without_permission_forbidden_to_view_invites()
     {
-        $this->actingAs($this->userDoe());
+        $this->actingAs($this->doe);
 
         $this->getJson(route('api.messenger.threads.invites.index', [
             'thread' => $this->group->id,
@@ -78,17 +79,15 @@ class InvitesTest extends FeatureTestCase
     /** @test */
     public function participant_with_permission_can_view_invites()
     {
-        $doe = $this->userDoe();
-
         $this->group->participants()
-            ->where('owner_id', '=', $doe->getKey())
-            ->where('owner_type', '=', get_class($doe))
+            ->where('owner_id', '=', $this->doe->getKey())
+            ->where('owner_type', '=', get_class($this->doe))
             ->first()
             ->update([
                 'manage_invites' => true,
             ]);
 
-        $this->actingAs($doe);
+        $this->actingAs($this->doe);
 
         $this->getJson(route('api.messenger.threads.invites.index', [
             'thread' => $this->group->id,
@@ -100,7 +99,7 @@ class InvitesTest extends FeatureTestCase
     /** @test */
     public function admin_can_view_invites()
     {
-        $this->actingAs($this->userTippin());
+        $this->actingAs($this->tippin);
 
         $this->getJson(route('api.messenger.threads.invites.index', [
             'thread' => $this->group->id,
@@ -128,7 +127,7 @@ class InvitesTest extends FeatureTestCase
             'uses' => 1,
         ]);
 
-        $this->actingAs($this->userTippin());
+        $this->actingAs($this->tippin);
 
         $this->getJson(route('api.messenger.threads.invites.index', [
             'thread' => $this->group->id,
@@ -142,7 +141,7 @@ class InvitesTest extends FeatureTestCase
     {
         $this->travel(2)->hours();
 
-        $this->actingAs($this->userTippin());
+        $this->actingAs($this->tippin);
 
         $this->getJson(route('api.messenger.threads.invites.index', [
             'thread' => $this->group->id,
@@ -158,7 +157,7 @@ class InvitesTest extends FeatureTestCase
             'invitations' => false,
         ]);
 
-        $this->actingAs($this->userTippin());
+        $this->actingAs($this->tippin);
 
         $this->getJson(route('api.messenger.threads.invites.index', [
             'thread' => $this->group->id,
@@ -169,7 +168,7 @@ class InvitesTest extends FeatureTestCase
     /** @test */
     public function participant_without_permission_forbidden_to_archive_invite()
     {
-        $this->actingAs($this->userDoe());
+        $this->actingAs($this->doe);
 
         $this->deleteJson(route('api.messenger.threads.invites.destroy', [
             'thread' => $this->group->id,
@@ -185,7 +184,7 @@ class InvitesTest extends FeatureTestCase
             InviteArchivedEvent::class,
         ]);
 
-        $this->actingAs($this->userTippin());
+        $this->actingAs($this->tippin);
 
         $this->deleteJson(route('api.messenger.threads.invites.destroy', [
             'thread' => $this->group->id,
@@ -201,17 +200,15 @@ class InvitesTest extends FeatureTestCase
             InviteArchivedEvent::class,
         ]);
 
-        $doe = $this->userDoe();
-
         $this->group->participants()
-            ->where('owner_id', '=', $doe->getKey())
-            ->where('owner_type', '=', get_class($doe))
+            ->where('owner_id', '=', $this->doe->getKey())
+            ->where('owner_type', '=', get_class($this->doe))
             ->first()
             ->update([
                 'manage_invites' => true,
             ]);
 
-        $this->actingAs($doe);
+        $this->actingAs($this->doe);
 
         $this->deleteJson(route('api.messenger.threads.invites.destroy', [
             'thread' => $this->group->id,
@@ -227,7 +224,7 @@ class InvitesTest extends FeatureTestCase
             NewInviteEvent::class,
         ]);
 
-        $this->actingAs($this->userTippin());
+        $this->actingAs($this->tippin);
 
         $this->postJson(route('api.messenger.threads.invites.store', [
             'thread' => $this->group->id,
@@ -251,17 +248,15 @@ class InvitesTest extends FeatureTestCase
             NewInviteEvent::class,
         ]);
 
-        $doe = $this->userDoe();
-
         $this->group->participants()
-            ->where('owner_id', '=', $doe->getKey())
-            ->where('owner_type', '=', get_class($doe))
+            ->where('owner_id', '=', $this->doe->getKey())
+            ->where('owner_type', '=', get_class($this->doe))
             ->first()
             ->update([
                 'manage_invites' => true,
             ]);
 
-        $this->actingAs($doe);
+        $this->actingAs($this->doe);
 
         $this->postJson(route('api.messenger.threads.invites.store', [
             'thread' => $this->group->id,
@@ -277,7 +272,7 @@ class InvitesTest extends FeatureTestCase
     {
         Messenger::setThreadInvitesMaxCount(1);
 
-        $this->actingAs($this->userTippin());
+        $this->actingAs($this->tippin);
 
         $this->postJson(route('api.messenger.threads.invites.store', [
             'thread' => $this->group->id,
@@ -293,7 +288,7 @@ class InvitesTest extends FeatureTestCase
     {
         Messenger::setThreadInvites(false);
 
-        $this->actingAs($this->userTippin());
+        $this->actingAs($this->tippin);
 
         $this->postJson(route('api.messenger.threads.invites.store', [
             'thread' => $this->group->id,
@@ -313,7 +308,7 @@ class InvitesTest extends FeatureTestCase
      */
     public function create_invite_checks_values($expiresValue, $usesValue, $errors)
     {
-        $this->actingAs($this->userTippin());
+        $this->actingAs($this->tippin);
 
         $this->postJson(route('api.messenger.threads.invites.store', [
             'thread' => $this->group->id,
