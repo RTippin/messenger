2e8b6446061e6df2ac2018ee7fb5174fbacedb2e
Tippin <richard.tippin@gmail.com>
2021-01-21 06:41:46 -0500

    RecipientThreadLocatorTest.php created, passes

---
+++ tests/Messenger/RecipientThreadLocatorTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace RTippin\Messenger\Tests\Messenger;
+
+use RTippin\Messenger\Contracts\MessengerProvider;
+use RTippin\Messenger\Facades\Messenger;
+use RTippin\Messenger\Models\Thread;
+use RTippin\Messenger\RecipientThreadLocator;
+use RTippin\Messenger\Tests\FeatureTestCase;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class RecipientThreadLocatorTest extends FeatureTestCase
+{
+    private RecipientThreadLocator $locator;
+
+    private Thread $private;
+
+    private MessengerProvider $tippin;
+
+    private MessengerProvider $doe;
+
+    private MessengerProvider $developers;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->tippin = $this->userTippin();
+
+        $this->doe = $this->userDoe();
+
+        $this->developers = $this->companyDevelopers();
+
+        $this->private = $this->createPrivateThread($this->tippin, $this->doe);
+
+        $this->locator = app(RecipientThreadLocator::class);
+
+        Messenger::setProvider($this->tippin);
+    }
+
+    /** @test */
+    public function locator_returns_user_with_existing_thread_id()
+    {
+        $results = $this->locator->setAlias('user')->setId($this->doe->getKey())->locate();
+
+        $this->assertSame('John Doe', $results->getRecipient()->name());
+
+        $this->assertSame($this->private->id, $results->getThread()->id);
+    }
+
+    /** @test */
+    public function locator_returns_company_without_existing_thread_id()
+    {
+        $results = $this->locator->setAlias('company')->setId($this->developers->getKey())->locate();
+
+        $this->assertSame('Developers', $results->getRecipient()->name());
+
+        $this->assertNull($results->getThread());
+    }
+
+    /** @test */
+    public function locator_returns_no_results_with_invalid_alias()
+    {
+        $results = $this->locator->setAlias('undefined')->setId($this->doe->getKey())->locate();
+
+        $this->assertNull($results->getThread());
+
+        $this->assertNull($results->getRecipient());
+    }
+
+    /** @test */
+    public function locator_returns_no_results_with_invalid_id()
+    {
+        $results = $this->locator->setAlias('user')->setId(404)->locate();
+
+        $this->assertNull($results->getThread());
+
+        $this->assertNull($results->getRecipient());
+    }
+
+    /** @test */
+    public function locator_returns_no_results_when_searching_for_current_provider()
+    {
+        $results = $this->locator->setAlias('user')->setId($this->tippin->getKey())->locate();
+
+        $this->assertNull($results->getThread());
+
+        $this->assertNull($results->getRecipient());
+    }
+
+    /** @test */
+    public function locator_can_throw_exception()
+    {
+        $this->expectException(NotFoundHttpException::class);
+
+        $this->locator->throwNotFoundError();
+    }
+}
