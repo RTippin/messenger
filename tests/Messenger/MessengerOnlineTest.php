ee0334c7afdce0491e15d1845a1be5a39b6a7a66
Tippin <richard.tippin@gmail.com>
2021-01-23 04:55:56 -0500

    MessengerOnlineTest.php fully passes

--- tests/Messenger/MessengerOnlineTest.php
+++ tests/Messenger/MessengerOnlineTest.php
@@ -94,4 +94,216 @@ class MessengerOnlineTest extends FeatureTestCase
 
         $this->assertFalse(Cache::has("user:online:{$invalid->getKey()}"));
     }
+
+    /** @test */
+    public function messenger_removes_current_provider_online_cache_key()
+    {
+        Cache::put("user:online:{$this->tippin->getKey()}", 'online');
+
+        $this->messenger->setProvider($this->tippin);
+
+        $this->messenger->setProviderToOffline();
+
+        $this->assertFalse(Cache::has("user:online:{$this->tippin->getKey()}"));
+    }
+
+    /** @test */
+    public function messenger_removes_given_provider_online_cache_key()
+    {
+        Cache::put("user:online:{$this->doe->getKey()}", 'online');
+
+        $this->messenger->setProviderToOffline($this->doe);
+
+        $this->assertFalse(Cache::has("user:online:{$this->doe->getKey()}"));
+    }
+
+    /** @test */
+    public function messenger_sets_current_provider_away_cache_key()
+    {
+        $this->messenger->setProvider($this->tippin);
+
+        $this->messenger->setProviderToAway();
+
+        $this->assertTrue(Cache::has("user:online:{$this->tippin->getKey()}"));
+        $this->assertSame('away', Cache::get("user:online:{$this->tippin->getKey()}"));
+    }
+
+    /** @test */
+    public function messenger_sets_given_provider_away_cache_key()
+    {
+        $this->messenger->setProviderToAway($this->doe);
+
+        $this->assertTrue(Cache::has("user:online:{$this->doe->getKey()}"));
+        $this->assertSame('away', Cache::get("user:online:{$this->doe->getKey()}"));
+    }
+
+    /** @test */
+    public function messenger_doesnt_set_away_cache_key_when_provider_settings_offline()
+    {
+        DB::table('messengers')->update([
+            'online_status' => 0,
+        ]);
+
+        $this->messenger->setProviderToAway($this->tippin);
+
+        $this->assertFalse(Cache::has("user:online:{$this->tippin->getKey()}"));
+    }
+
+    /** @test */
+    public function messenger_doesnt_set_away_cache_key_when_disabled_from_config()
+    {
+        $this->messenger->setOnlineStatus(false);
+
+        $this->messenger->setProviderToAway($this->tippin);
+
+        $this->assertFalse(Cache::has("user:online:{$this->tippin->getKey()}"));
+    }
+
+    /** @test */
+    public function messenger_doesnt_set_away_cache_key_for_invalid_provider()
+    {
+        $invalid = new OtherModel([
+            'id' => 404,
+        ]);
+
+        $this->messenger->setProviderToAway($invalid);
+
+        $this->assertFalse(Cache::has("user:online:{$invalid->getKey()}"));
+    }
+
+    /** @test */
+    public function messenger_returns_set_provider_is_online()
+    {
+        Cache::put("user:online:{$this->tippin->getKey()}", 'online');
+
+        $this->messenger->setProvider($this->tippin);
+
+        $this->assertTrue($this->messenger->isProviderOnline());
+    }
+
+    /** @test */
+    public function messenger_returns_given_provider_is_online()
+    {
+        Cache::put("user:online:{$this->doe->getKey()}", 'online');
+
+        $this->assertTrue($this->messenger->isProviderOnline($this->doe));
+    }
+
+    /** @test */
+    public function messenger_returns_provider_is_not_online_when_disabled_from_config()
+    {
+        $this->messenger->setOnlineStatus(false);
+
+        Cache::put("user:online:{$this->tippin->getKey()}", 'online');
+
+        $this->assertFalse($this->messenger->isProviderOnline($this->tippin));
+    }
+
+    /** @test */
+    public function messenger_returns_invalid_provider_is_not_online()
+    {
+        $invalid = new OtherModel([
+            'id' => 404,
+        ]);
+
+        Cache::put("user:online:{$invalid->getKey()}", 'online');
+
+        $this->assertFalse($this->messenger->isProviderOnline($invalid));
+    }
+
+    /** @test */
+    public function messenger_returns_set_provider_is_away()
+    {
+        Cache::put("user:online:{$this->tippin->getKey()}", 'away');
+
+        $this->messenger->setProvider($this->tippin);
+
+        $this->assertTrue($this->messenger->isProviderAway());
+    }
+
+    /** @test */
+    public function messenger_returns_given_provider_is_away()
+    {
+        Cache::put("user:online:{$this->doe->getKey()}", 'away');
+
+        $this->assertTrue($this->messenger->isProviderAway($this->doe));
+    }
+
+    /** @test */
+    public function messenger_returns_provider_is_not_away_when_online()
+    {
+        Cache::put("user:online:{$this->doe->getKey()}", 'online');
+
+        $this->assertFalse($this->messenger->isProviderAway($this->doe));
+    }
+
+    /** @test */
+    public function messenger_returns_provider_is_not_away_when_disabled_from_config()
+    {
+        $this->messenger->setOnlineStatus(false);
+
+        Cache::put("user:online:{$this->tippin->getKey()}", 'away');
+
+        $this->assertFalse($this->messenger->isProviderAway($this->tippin));
+    }
+
+    /** @test */
+    public function messenger_returns_invalid_provider_is_not_away()
+    {
+        $invalid = new OtherModel([
+            'id' => 404,
+        ]);
+
+        Cache::put("user:online:{$invalid->getKey()}", 'away');
+
+        $this->assertFalse($this->messenger->isProviderAway($invalid));
+    }
+
+    /** @test */
+    public function messenger_returns_online_status_number_for_set_provider()
+    {
+        $this->messenger->setProvider($this->tippin);
+
+        $this->assertSame(0, $this->messenger->getProviderOnlineStatus());
+
+        Cache::put("user:online:{$this->tippin->getKey()}", 'online');
+
+        $this->assertSame(1, $this->messenger->getProviderOnlineStatus());
+
+        Cache::put("user:online:{$this->tippin->getKey()}", 'away');
+
+        $this->assertSame(2, $this->messenger->getProviderOnlineStatus());
+    }
+
+    /** @test */
+    public function messenger_returns_online_status_number_for_given_provider()
+    {
+        $this->assertSame(0, $this->messenger->getProviderOnlineStatus($this->doe));
+
+        Cache::put("user:online:{$this->doe->getKey()}", 'online');
+
+        $this->assertSame(1, $this->messenger->getProviderOnlineStatus($this->doe));
+
+        Cache::put("user:online:{$this->doe->getKey()}", 'away');
+
+        $this->assertSame(2, $this->messenger->getProviderOnlineStatus($this->doe));
+    }
+
+    /** @test */
+    public function messenger_returns_online_status_number_for_invalid_provider_always_offline()
+    {
+        $invalid = new OtherModel([
+            'id' => 404,
+        ]);
+
+        $this->assertSame(0, $this->messenger->getProviderOnlineStatus($invalid));
+
+        Cache::put("user:online:{$invalid->getKey()}", 'online');
+
+        $this->assertSame(0, $this->messenger->getProviderOnlineStatus($invalid));
+
+        Cache::put("user:online:{$invalid->getKey()}", 'away');
+
+        $this->assertSame(0, $this->messenger->getProviderOnlineStatus($invalid));
+    }
 }
