5b0c1489871df0039ce89e74379c7af83e7b2936
Tippin <richard.tippin@gmail.com>
2021-02-01 18:07:34 -0500

    RateLimitersTest.php passes

--- tests/Middleware/RateLimitersTest.php
+++ tests/Middleware/RateLimitersTest.php
@@ -2,8 +2,11 @@
 
 namespace RTippin\Messenger\Tests\Middleware;
 
+use Illuminate\Http\UploadedFile;
 use Illuminate\Routing\Middleware\ThrottleRequests;
+use Illuminate\Support\Facades\Storage;
 use RTippin\Messenger\Contracts\MessengerProvider;
+use RTippin\Messenger\Facades\Messenger;
 use RTippin\Messenger\Tests\FeatureTestCase;
 
 class RateLimitersTest extends FeatureTestCase
@@ -33,6 +36,24 @@ class RateLimitersTest extends FeatureTestCase
         $this->assertEquals(120, $limit);
 
         $this->assertEquals(119, $remaining);
+    }
+
+    /** @test */
+    public function setting_limit_to_zero_results_in_unlimited_request_per_minute()
+    {
+        Messenger::setApiRateLimit(0);
+
+        $this->actingAs($this->tippin);
+
+        $response = $this->getJson(route('api.messenger.threads.index'));
+
+        $limit = $response->headers->get('X-Ratelimit-Limit');
+
+        $remaining = $response->headers->get('X-RateLimit-Remaining');
+
+        $this->assertNull($limit);
+
+        $this->assertNull($remaining);
     }
 
     /** @test */
@@ -72,5 +93,55 @@ class RateLimitersTest extends FeatureTestCase
         $this->assertEquals(60, $limit);
 
         $this->assertEquals(59, $remaining);
+    }
+
+    /** @test */
+    public function store_image_message_api_limits_request_10_per_minute()
+    {
+        Storage::fake(Messenger::getThreadStorage('disk'));
+
+        $group = $this->createGroupThread($this->tippin);
+
+        $this->actingAs($this->tippin);
+
+        $response = $this->postJson(route('api.messenger.threads.images.store', [
+            'thread' => $group->id,
+        ]), [
+            'image' => UploadedFile::fake()->image('picture.jpg'),
+            'temporary_id' => '123-456-789',
+        ]);
+
+        $limit = $response->headers->get('X-Ratelimit-Limit');
+
+        $remaining = $response->headers->get('X-RateLimit-Remaining');
+
+        $this->assertEquals(10, $limit);
+
+        $this->assertEquals(9, $remaining);
+    }
+
+    /** @test */
+    public function store_document_message_api_limits_request_10_per_minute()
+    {
+        Storage::fake(Messenger::getThreadStorage('disk'));
+
+        $group = $this->createGroupThread($this->tippin);
+
+        $this->actingAs($this->tippin);
+
+        $response = $this->postJson(route('api.messenger.threads.documents.store', [
+            'thread' => $group->id,
+        ]), [
+            'document' => UploadedFile::fake()->create('test.pdf', 500, 'application/pdf'),
+            'temporary_id' => '123-456-789',
+        ]);
+
+        $limit = $response->headers->get('X-Ratelimit-Limit');
+
+        $remaining = $response->headers->get('X-RateLimit-Remaining');
+
+        $this->assertEquals(10, $limit);
+
+        $this->assertEquals(9, $remaining);
     }
 }
