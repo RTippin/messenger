daa7158bedf343158960df36885fee44bc453d78
Tippin <richard.tippin@gmail.com>
2021-02-13 04:39:09 -0500

    MessageEditTest.php passes.

---
+++ tests/Models/MessageEditTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace RTippin\Messenger\Tests\Models;
+
+use Illuminate\Support\Carbon;
+use RTippin\Messenger\Contracts\MessengerProvider;
+use RTippin\Messenger\Models\Message;
+use RTippin\Messenger\Models\MessageEdit;
+use RTippin\Messenger\Tests\FeatureTestCase;
+
+class MessageEditTest extends FeatureTestCase
+{
+    private MessengerProvider $tippin;
+
+    private Message $message;
+
+    private MessageEdit $edited;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->tippin = $this->userTippin();
+
+        $group = $this->createGroupThread($this->tippin);
+
+        $this->message = $this->createMessage($group, $this->tippin);
+
+        $this->edited = $this->message->edits()->create([
+            'body' => 'EDITED',
+            'edited_at' => now(),
+        ]);
+    }
+
+    /** @test */
+    public function message_edit_exists()
+    {
+        $this->assertDatabaseCount('message_edits', 1);
+        $this->assertDatabaseHas('message_edits', [
+            'id' => $this->edited->id,
+        ]);
+        $this->assertInstanceOf(MessageEdit::class, $this->edited);
+    }
+
+    /** @test */
+    public function message_edit_attributes_casted()
+    {
+        $this->assertInstanceOf(Carbon::class, $this->edited->edited_at);
+    }
+
+    /** @test */
+    public function message_edit_has_relation()
+    {
+        $this->assertSame($this->message->id, $this->edited->message->id);
+        $this->assertInstanceOf(Message::class, $this->edited->message);
+    }
+}
