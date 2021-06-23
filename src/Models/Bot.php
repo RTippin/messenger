752757f9b96ed935bea3b66c80161ff10147b8a1
Tippin <richard.tippin@gmail.com>
2021-06-23 01:55:32 -0400

    styleci

--- src/Models/Bot.php
+++ src/Models/Bot.php
@@ -2,7 +2,6 @@
 
 namespace RTippin\Messenger\Models;
 
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
