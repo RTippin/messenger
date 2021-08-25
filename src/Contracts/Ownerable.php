d99636d21f76d84e59b850ff79390185951a9a06
Tippin <richard.tippin@gmail.com>
2021-08-24 22:29:21 -0400

    Update missing docblocks.

--- src/Contracts/Ownerable.php
+++ src/Contracts/Ownerable.php
@@ -2,8 +2,15 @@
 
 namespace RTippin\Messenger\Contracts;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
+/**
+ * @property string|int $owner_id
+ * @property string $owner_type
+ * @property-read Model|MessengerProvider $owner
+ * @mixin Model|\Eloquent
+ */
 interface Ownerable
 {
     /**
--- src/Models/MessageReaction.php
+++ src/Models/MessageReaction.php
@@ -18,13 +18,10 @@ use RTippin\Messenger\Traits\Uuids;
 
 /**
  * @property string $id
- * @property string $owner_type
- * @property string|int $owner_id
  * @property string $message_id
  * @property string $reaction
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property-read \RTippin\Messenger\Models\Message $message
- * @property-read Model|MessengerProvider $owner
  * @mixin Model|\Eloquent
  * @method static Builder|MessageReaction whereReaction(string $reaction)
  * @method static Builder|MessageReaction notReaction(string $reaction)
