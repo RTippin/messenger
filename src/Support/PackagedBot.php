cf93cabea074db4a6c6a04e65d2f9f592ba9f38b
Tippin <richard.tippin@gmail.com>
2021-12-01 22:39:49 -0500

    Update docblock wording.

--- src/Support/PackagedBot.php
+++ src/Support/PackagedBot.php
@@ -20,16 +20,44 @@ abstract class PackagedBot
      * - 'name' displayed to the frontend.
      * - 'description' displayed to the frontend.
      * Optional:
-     * 'avatar' Set the path to your packaged bots avatar. Upon installation, the avatar will be stored for the bot.
-     * 'cooldown' The bots' cooldown. Default of 0.
-     * 'enabled' Whether the bot starts out as enabled or disabled. Default is true.
-     * 'hide_actions' Whether the bots' actions start out as hidden. Default is false.
+     * - 'avatar' Set the path to your packaged bots avatar. Upon installation, the avatar will be stored for the bot.
+     * - 'cooldown' The bots' cooldown. Default of 0.
+     * - 'enabled' Whether the bot starts out as enabled or disabled. Default is true.
+     * - 'hide_actions' Whether the bots' actions start out as hidden. Default is false.
      *
      * @return array{alias: string, name: string, description: string, avatar: string, cooldown: int, enabled: bool, hide_actions: bool}
      */
     abstract public static function getSettings(): array;
 
     /**
+     * Return the listing of bot handler classes you want to be bundled with this install.
+     * The array keys must be the bot handler class. For handlers that require you to set
+     * properties, or that you want to override certain defaults, you must define them as
+     * the value of the handler class key represented as an associative array. If you want
+     * the handler to be installed multiple times with different parameters, you can define
+     * an array of arrays as the value. The key/values of your parameters must match the
+     * default for a BotAction model, as well as include any parameters that are defined on
+     * the handlers rules for serializing a payload.
+     * BotAction handler keys include:
+     * - 'enabled' Default of true.
+     * - 'cooldown' : Default of 30.
+     * - 'triggers' : No default.
+     * - 'admin_only' : Default of false.
+     * - 'match' : No Default.
+     *
+     * Any parameters that are already defined in the bot handler class cannot be overridden.
+     * While installing, each handler will pass through a validation process, and will be
+     * discarded if it fails validating.
+     *
+     * Handler with and without parameters:
+     * - [HandlerOne::class => ['triggers' => ['one', 'two']], HandlerTwo::class, ...]
+     *
+     * Handler with required parameters and custom rules:
+     * - [HandlerOne::class => ['triggers' => ['one'], 'cooldown' => 60, 'custom_rule' => true, 'match' => 'exact']]
+     *
+     * Multiple of the same handler:
+     * - [HandlerTwo::class => [['triggers' => ['one']], ['triggers' => ['two']]]]
+     *
      * @return array
      */
     abstract public static function installs(): array;
